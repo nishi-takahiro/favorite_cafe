ea713998e1723247e7ba4c12ec9c6e588c26ed60
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2022-08-04 12:04:16 +0000

    Google map表記検索機能

--- app/controllers/public/stores_controller.rb
+++ app/controllers/public/stores_controller.rb
@@ -1,5 +1,25 @@
 class Public::StoresController < ApplicationController
+  require 'net/http'
+  require 'uri'
+  require 'json'
+  def search
+    key = "9d0e3bd839c5a4d3"
+    lat = '35.658'
+    lng = '139.7016'
+    range = 1
+    api = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{key}&lat=#{lat}&lng=#{lng}&range=#{range}&order=1&format=json")
+    json = Net::HTTP.get(api)
+    @result = JSON.parse(json)    
+  end
+
   def new
+    key = "9d0e3bd839c5a4d3"
+    lat = '35.658'
+    lng = '139.7016'
+    range = 1
+    api = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{key}&lat=#{lat}&lng=#{lng}&range=#{range}&order=1&format=json")
+    json = Net::HTTP.get(api)
+    @result = JSON.parse(json)    
   end
 
   def edit
