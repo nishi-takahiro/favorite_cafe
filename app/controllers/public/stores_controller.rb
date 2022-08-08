3f32ad36693df9e1e231106c6f797f3ae9c7cc47
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2022-08-08 03:06:03 +0000

    jsonからのデータ取り出し

--- app/controllers/public/stores_controller.rb
+++ app/controllers/public/stores_controller.rb
@@ -26,6 +26,16 @@ class Public::StoresController < ApplicationController
   end
 
   def index
+    key = "9d0e3bd839c5a4d3"
+    lat = '34.704649'
+    lng = '135.499200'
+
+    range = 1
+    api = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{key}&lat=#{lat}&lng=#{lng}&range=#{range}&order=1&format=json")
+    json = Net::HTTP.get(api)
+    @result = JSON.parse(json)
+    @result_data = JSON.parse(json, symbolize_names: true)
+    @result_data1 = @result_data[:results][:shop]
   end
 
   def show
