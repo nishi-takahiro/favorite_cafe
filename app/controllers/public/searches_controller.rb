a9be573d909aeee84bbc5fecb57fa77a2ed4373a
nishi-takahiro <103094375+nishi-takahiro@users.noreply.github.com>
2022-08-27 13:25:57 +0000

    検索機能修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,13 +1,25 @@
 class Public::SearchesController < ApplicationController
     
-def search
-  @range = params[:range]
-    
-  if @range == "Article"
-   @article = Article.looks(params[:search], params[:word])
+ def search
+  if params[:search].present?
+     
+   @range = params[:range]
+    # binding.pry
+   if @range == "Article"
+    @article = Article.looks(params[:search], params[:word]).order(created_at: :desc)
+   elsif @range == "User"
+    @user = User.looks(params[:search], params[:word])
+   end
+     
+  elsif params[:tag_id].present?
+   @tag = Tag.find(params[:tag_id])
+   articles = @tag.articles.order(created_at: :desc)
+   @article = Kaminari.paginate_array(articles).page(params[:page]).per(10)
   else
-   @user = User.looks(params[:search], params[:word])
+   # articles = Article.all.order(created_at: :desc)
+   # @tag_article = Kaminari.paginate_array(articles).page(params[:page]).per(10)
   end
-end
+   # @tag_lists = Tag.all
+ end
 
 end
