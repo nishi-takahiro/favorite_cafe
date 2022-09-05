cdeb61aaa765df60aa085c61d0c8582d469b96c5
nishi-takahiro <wakuwaku090zoo@gmail.com>
2022-09-05 11:16:04 +0000

    adminコントローラ変更

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,11 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
-    # 全投稿を新しい順番に表示をする
-    @articles = Article.all.order(created_at: :desc).page(params[:page]).per(20)
+    begin
+      # 全投稿を新しい順番に表示をする
+      @articles = Article.all.order(created_at: :desc).page(params[:page]).per(20)
+    rescue
+      redirect_to root
+    end
   end
 end
